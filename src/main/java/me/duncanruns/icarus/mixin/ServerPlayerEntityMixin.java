a5cd9b839cbaea8a67855d9cac9f44b633a9c00a
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2021-12-16 15:13:04 -0400

    1.17

--- src/main/java/me/duncanruns/icarus/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/me/duncanruns/icarus/mixin/ServerPlayerEntityMixin.java
@@ -3,7 +3,16 @@ package me.duncanruns.icarus.mixin;
 import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-
+import me.duncanruns.icarus.Icarus;
+import net.minecraft.command.argument.ItemStackArgumentType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.ServerStatHandler;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,26 +21,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import me.duncanruns.icarus.Icarus;
-import net.minecraft.command.arguments.ItemStackArgumentType;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.stat.ServerStatHandler;
-import net.minecraft.stat.Stats;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
-
-    public ServerPlayerEntityMixin(World world, BlockPos blockPos, GameProfile gameProfile) {
-        super(world, blockPos, gameProfile);
-    }
 
     @Shadow
     @Final
     private ServerStatHandler statHandler;
+
+    public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
+        super(world, pos, yaw, profile);
+    }
 
     private ItemStack itemStackFromString(String string, int count) throws CommandSyntaxException {
         return new ItemStackArgumentType().parse(new StringReader(string)).createStack(count, false);
@@ -39,14 +38,15 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
     @Inject(at = @At("TAIL"), method = "<init>")
     private void init(CallbackInfo info) throws CommandSyntaxException {
-        if (statHandler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_ONE_MINUTE)) == 0) {
-            
+        if (statHandler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_TIME)) == 0) {
+
             Icarus.log(Level.INFO, "New player detected, activating Icarus.");
 
             //Item strings copy & pasted directly from jojoe's datapack
             ItemStack wings = itemStackFromString("minecraft:elytra{Unbreakable:1b}", 1);
             ItemStack rockets = itemStackFromString("minecraft:firework_rocket{Fireworks:{Flight:3b}}", 64);
 
+            PlayerInventory inventory = getInventory();
             inventory.armor.set(2, wings);
             inventory.main.set(0, rockets);
         }
