e05466537d2e716bf2a516446a8e0d4b93e6fc82
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2021-12-16 15:23:30 -0400

    Update all this shit

--- src/main/java/me/duncanruns/icarus/Icarus.java
+++ src/main/java/me/duncanruns/icarus/Icarus.java
@@ -1,25 +1,23 @@
 package me.duncanruns.icarus;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Icarus implements ModInitializer {
 
-    public static Logger LOGGER = LogManager.getLogger();
-
     public static final String MOD_ID = "icarus";
     public static final String MOD_NAME = "Icarus";
+    public static Logger LOGGER = LogManager.getLogger();
+
+    public static void log(Level level, String message) {
+        LOGGER.log(level, "[" + MOD_NAME + "] " + message);
+    }
 
     @Override
     public void onInitialize() {
         log(Level.INFO, "Initializing");
-    }
-
-    public static void log(Level level, String message) {
-        LOGGER.log(level, "[" + MOD_NAME + "] " + message);
     }
 
 }
--- src/main/java/me/duncanruns/icarus/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/me/duncanruns/icarus/mixin/ServerPlayerEntityMixin.java
@@ -3,15 +3,6 @@ package me.duncanruns.icarus.mixin;
 import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-
-import org.apache.logging.log4j.Level;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import me.duncanruns.icarus.Icarus;
 import net.minecraft.command.arguments.ItemStackArgumentType;
 import net.minecraft.entity.player.PlayerEntity;
@@ -21,17 +12,24 @@ import net.minecraft.stat.ServerStatHandler;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.apache.logging.log4j.Level;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
-    public ServerPlayerEntityMixin(World world, BlockPos blockPos, GameProfile gameProfile) {
-        super(world, blockPos, gameProfile);
-    }
-
     @Shadow
     @Final
     private ServerStatHandler statHandler;
+
+    public ServerPlayerEntityMixin(World world, BlockPos blockPos, GameProfile gameProfile) {
+        super(world, blockPos, gameProfile);
+    }
 
     private ItemStack itemStackFromString(String string, int count) throws CommandSyntaxException {
         return new ItemStackArgumentType().parse(new StringReader(string)).createStack(count, false);
@@ -40,7 +38,7 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
     @Inject(at = @At("TAIL"), method = "<init>")
     private void init(CallbackInfo info) throws CommandSyntaxException {
         if (statHandler.getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_ONE_MINUTE)) == 0) {
-            
+
             Icarus.log(Level.INFO, "New player detected, activating Icarus.");
 
             //Item strings copy & pasted directly from jojoe's datapack
